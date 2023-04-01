a48997dd72ec3ade80ff507fd69007f96a3a78be
Ali Jabir <syedalijabir@gmail.com>
2023-04-01 07:50:05 +0100

    fix: remove comparison bug + add local region support

--- terraform/aws-ecs-application/app/scripts/run.sh
+++ terraform/aws-ecs-application/app/scripts/run.sh
@@ -3,9 +3,11 @@ set -x
 set +e
 
 metadata=$(curl --max-time 1 -s ${ECS_CONTAINER_METADATA_URI_V4}/task)
-if [[ $?=='0' ]]; then
+if [[ $? -eq 0 ]]; then
   az=$(echo "$metadata" | jq '.AvailabilityZone' | tr -d '"')
   export REGION=${az}
+else
+  export REGION=local
 fi
 
 set -e
